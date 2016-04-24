afde0af389d637c5af14315a55fcb7152266bb7c
Mac McEacharn <mmceacharn@pros.com>
2016-04-24 11:31:10 -0500

    completed plot 3

---
+++ plot3.R
@@ -0,0 +1,20 @@
+source("getdata.R")
+
+png(
+  filename = "plot3.png", bg = "white", width = 480, height = 480, units = "px"
+)
+
+plot(
+  DateTime, Sub_metering_1,type = "l", col = "black",xlab = "", ylab = "Energy sub metering"
+)
+
+lines(DateTime, Sub_metering_2, col = "red")
+
+lines(DateTime, Sub_metering_3, col = "blue")
+
+legend(
+  "topright", lwd=1, border = "black", bg = "white", col = c("black", "red", "blue"), 
+  c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+)
+
+dev.off()
