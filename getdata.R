e94f8474146dc4f781619751f55bb73893d268ea
Mac McEacharn <mmceacharn@pros.com>
2016-04-24 11:04:30 -0500

    completed plot 1

---
+++ getdata.R
@@ -0,0 +1,32 @@
+# function to build path to files from a list of folders and a file name
+buildPath <- function(...) {
+  paste(... , sep = .Platform$file.sep)
+}
+
+dataFolder <- "./data"
+zipFileName <- buildPath(dataFolder, "dataset.zip")
+fileName <- buildPath(dataFolder, "household_power_consumption.txt")
+
+# create the data folder if it does not exists
+if (!file.exists(dataFolder)) {
+  dir.create(dataFolder)
+}
+
+# download and unzip data
+if (!file.exists(zipFileName)) {
+  sourceData <-
+    "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(sourceData, destfile = zipFileName, method = "curl")
+}
+
+unzip(zipFileName, overwrite = TRUE, exdir = dataFolder)
+
+# load the data for two specific days
+fileData <- read.table(fileName,header = TRUE, sep = ";", na = "?")
+attach(fileData)
+twoDaysData <- Date == "1/2/2007" | Date == "2/2/2007"
+rangeData <- data[twoDaysData,]
+attach(rangeData)
+
+rangeData$DateTime <-
+  strptime(paste(Date, Time), "%d/%m/%Y %H:%M:%S")
---
+++ plot1.R
@@ -0,0 +1,14 @@
+source("getdata.R")
+
+globalActivePowerData <- as.numeric(rangeData$Global_active_power)
+
+png(
+  filename = "plot1.png", bg = "white",width = 480, height = 480, units = "px"
+)
+
+hist(
+  globalActivePowerData, col = "red", main = "Global Active Power",
+  xlab = "Global Active Power (kilowatts)",breaks = 12, ylim = c(0, 1200)
+)
+
+dev.off()
